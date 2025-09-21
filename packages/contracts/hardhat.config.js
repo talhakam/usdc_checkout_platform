7e48537b8cf54eec3599385996c78509e821d707
talhakam <talhakamran17@gmail.com>
2025-09-21 15:51:12 +0300

    Slither Report

--- packages/contracts/hardhat.config.js
+++ packages/contracts/hardhat.config.js
@@ -3,19 +3,25 @@ require("@nomicfoundation/hardhat-toolbox");
 
 const { PRIVATE_KEY, ALCHEMY_AMOY_URL } = process.env;
 
+const networks = {
+  hardhat: {}
+};
+
+// Only add when the ALCHEMY_AMOY_URL env var is set.
+if (ALCHEMY_AMOY_URL) {
+  networks.amoy = {
+    url: ALCHEMY_AMOY_URL,
+    chainId: 80002,
+    accounts: PRIVATE_KEY ? [PRIVATE_KEY] : []
+  };
+}
+
 module.exports = {
   solidity: {
     version: "0.8.20",
     settings: { optimizer: { enabled: true, runs: 200 } }
   },
-  networks: {
-    hardhat: {},
-    amoy: {
-      url: ALCHEMY_AMOY_URL,
-      chainId: 80002,
-      accounts: PRIVATE_KEY ? [PRIVATE_KEY] : []
-    }
-  },
+  networks,
   etherscan: {
     apiKey: process.env.ETHERSCAN_API_KEY
   },
